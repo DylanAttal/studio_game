8bb39ab2ba0293dc81e283ed6594c37e0aba044a
Dylan Attal <attal.dylan@gmail.com>
2021-12-13 12:14:14 -0500

    displays player 3 name and health centered with asteriks

--- studio_game.rb
+++ studio_game.rb
@@ -4,9 +4,12 @@ name_3 = "moe"
 
 health_1 = 60
 health_2 = 125
+health_3 = 100
 
 name_1 = name_1.capitalize
 name_2 = name_2.upcase
+name_3 = name_3.capitalize
 
 puts "#{name_1} has a health of #{health_1}"
 puts "#{name_2} has a health of #{health_2}"
+puts "#{name_3} has a health of #{health_3}".center(40, "*")
