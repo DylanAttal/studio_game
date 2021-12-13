4cc5e386abc71a352a38c7f70fe51d2285ceb117
Dylan Attal <attal.dylan@gmail.com>
2021-12-13 12:03:18 -0500

    displays player 2 health with fully capitalized name

--- studio_game.rb
+++ studio_game.rb
@@ -3,7 +3,10 @@ name_2 = "curly"
 name_3 = "moe"
 
 health_1 = 60
+health_2 = 125
 
 name_1 = name_1.capitalize
+name_2 = name_2.upcase
 
 puts "#{name_1} has a health of #{health_1}"
+puts "#{name_2} has a health of #{health_2}"
