22abdd51581c4b2a450df953627b7c273aae2add
Dylan Attal <attal.dylan@gmail.com>
2022-01-10 13:30:54 -0500

    Makes rounds and prints final stats.

--- game.rb
+++ game.rb
@@ -13,16 +13,45 @@ class Game
     @players.push(player)
   end
 
-  def play
+  def play(rounds)
     puts "There are #{@players.size} players in #{@title}:"
 
     @players.each do |player|
       puts player
     end
 
-    @players.each do |player|
-      GameTurn.take_turn(player)
-      puts player
+    1.upto(rounds) do |round|
+      puts "\nRound #{round}"
+      @players.each do |player|
+        GameTurn.take_turn(player)
+        puts player
+      end
+    end
+  end
+
+  def print_name_and_health(player)
+    puts "#{player.name} (#{player.health})"
+  end
+
+  def print_stats
+    strong_players, wimpy_players = @players.partition { |p| p.strong? }
+
+    puts "Knuckleheads Statistics:"
+
+    puts "#{strong_players.length} strong players:"
+    strong_players.each do |player|
+      print_name_and_health(player)
+    end
+
+    puts "#{wimpy_players.length} wimpy players:"
+    wimpy_players.each do |p|
+      print_name_and_health(player)
+    end
+
+    sorted_players = @players.sort
+    sorted_players.each do |player|
+      formatted_name = player.name.ljust(20, '.')
+      puts "#{formatted_name} #{player.score}"
     end
   end
 end
