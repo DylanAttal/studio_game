5d13a6caada416effb170d0761dee923f41e8551
Dylan Attal <attal.dylan@gmail.com>
2022-01-16 15:15:58 -0500

    Calculate Player score using Treasure value.

--- player.rb
+++ player.rb
@@ -2,13 +2,14 @@ class Player
   def initialize(name, health = 100)
     @name = name.capitalize
     @health = health
+    @found_treasures = Hash.new(0)
   end
 
   attr_accessor :name
   attr_reader :health
 
   def to_s
-    "I'm #{@name} with a health of #{health} and a score of #{score}."
+    "I'm #{@name} with a health = #{@health}, points = #{points}, and score = #{score}."
   end
 
   def w00t
@@ -22,7 +23,7 @@ class Player
   end
 
   def score
-    @health + @name.length
+    @health + points
   end
 
   def strong?
@@ -31,6 +32,16 @@ class Player
 
   def <=>(other)
     other.score <=> score
+  end
+
+  def found_treasure(treasure)
+    @found_treasures[treasure.name] += treasure.points
+    puts "#{@name} found a #{treasure.name} worth #{treasure.points} points."
+    puts "#{@name}'s treasures: #{@found_treasures}"
+  end
+
+  def points
+     @found_treasures.values.reduce(0, :+)
   end
 end
 
